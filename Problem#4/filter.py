91f5bcd6bf0b5803ce734a0f6d5e958f1fc5f402
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-07 15:10:03 -0600

    Did it again

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,12 +1,20 @@
-def process_and_print(input_string):
+def process_and_print(user_input):
       # Split into separate strings
-  split = input_string.split()
-    # Convert strings to integers and filter out negative values
-    input_data = 
+    split = user_input.split()
+        # Convert strings to integers and filter out negative values
+    input_data = []
+    for i in split:
+        if int(i) < 0:
+            input_data.append(int(i))
 
     # Sort integers in reverse order
-  
+    input_data.sort(reverse=True)
+    #input_data.reverse()
     # Print sorted integers
+    for values in input_data:
+        print(values, end=' ')
+
+   
     
     
 
