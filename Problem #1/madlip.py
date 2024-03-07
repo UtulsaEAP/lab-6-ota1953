965ee74ee8a3b7d3d11ace671389d9f7d68633d9
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-06 22:10:46 -0600

    Try 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,9 +1,14 @@
+# Owen Anderson THUR 2:00 - 3:15
 def food_input():
     user_input = input()
     tokens = user_input.split()
-    #type you while  loop here
+    while tokens[0] != "done" and tokens[1] != 0:
+        print("Eating "+tokens[1]+ " "+ tokens[0]+" a day keeps you happy and healthy.")
+        user_input = input()
+        tokens = user_input.split()
+        return
 
-    
 
 if __name__ == "__main__":
     food_input()
+    
