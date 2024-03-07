822fadf3b1bb0bdef69df3ad09b4b3af05a38cd3
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-07 10:11:08 -0600

    did it

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,14 @@
 def check_palindrome(user_input):
- #type your code here
+    new_string = ""
+    index = len(user_input)
+    while index > 0:
+        new_string += user_input[index-1]
+        index -= 1
+    if user_input == new_string:
+        return "palindrome: " + new_string
+    else:
+        return "not a palindrome: " + user_input
+
 if __name__ == "__main__":
     user_input = input()
-    check_palindrome(user_input)
+    print(check_palindrome(user_input))
