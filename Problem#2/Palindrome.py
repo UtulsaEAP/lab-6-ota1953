a68d99da9e108c55aab47ca61bfc8cbf2be26396
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-08 15:28:55 -0600

    23

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -3,11 +3,10 @@ Name: Owen Anderson
 Lab Time: THUR 2:00 - 3:15
 '''
 def check_palindrome(user_input):
-    new_string = ""
-    index = len(user_input)
-    while index > 0:
-        new_string += user_input[index-1]
-        index -= 1
+    new_string =""
+    for i in user_input:
+        new_string += i[::-1]
+    
     if user_input == new_string:
         return "palindrome: " + new_string
     else:
