c76731e1cd6e6a34cd701aa1306204fc292e38b1
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-03-07 21:01:50 -0600

    WHY ISNT IT WORKING

--- Problem#7/carwash.py
+++ Problem#7/carwash.py
@@ -1,9 +1,34 @@
+'''
+Name: Owen Anderson 
+Lab Time: THUR 2:00 - 3:15
+'''
 def calculate_car_wash_price(service_choice1, service_choice2):
     services = {'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5}
     base_wash = 10
     total = 0
    
    #type your code here 
+    print("ZyCar Wash")
+    print("Base car wash - $"+str(base_wash))
+    if service_choice1 == "-":
+        pass
+    elif service_choice1 in services:
+        print(service_choice1+' - $'+str(services[service_choice1]))
+
+    if service_choice2 == "-":
+        pass
+    elif service_choice2 in services:
+        print(service_choice2+' - $',str(services[service_choice2]))
+
+    if service_choice2 == "-":
+        if service_choice1 == "-":
+            total = base_wash
+        elif service_choice1 != "-":
+            total = base_wash+services[service_choice1]
+    else:
+        total = base_wash+services[service_choice1]+services[service_choice2]
+    print("-----")
+    print("Total price: $"+str(total))
 
     
 if __name__ == '__main__':
